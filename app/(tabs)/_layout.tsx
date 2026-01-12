77cbfaf2fd8e0bdc32c12dbf11e4429441579544
AlexanderHobbs <hobbs.l.alexander@gmail.com>
2026-01-12 13:33:49 -0600

    nav bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,15 +1,44 @@
 import { Tabs } from "expo-router";
+import {Ionicons} from '@expo/vector-icons';
 
 export default function TabLayout() {
-  return 
+  return (
   
-  <Tabs
-  screenOptions = {{
-    headerShown: false,
-    
-  }}
-  >
-  
+    <Tabs
+      screenOptions = {{
+        headerShown: false,
+        tabBarActiveTintColor: 'gray',
+      }}
+    >
+      <Tabs.Screen
+        name = "discover"
+        options = {{
+          title: 'Discover',
+          tabBarIcon: ({color, size}) => (
+            <Ionicons name = "musical-notes" color = {color} size = {size} />
+          ),
+        }}
+      />
 
-  </Tabs>;
+      <Tabs.Screen
+        name = "map"
+        options = {{
+          title: 'Map',
+          tabBarIcon: ({color, size}) => (
+            <Ionicons name = "map" color = {color} size = {size} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name = "saved"
+        options = {{
+          title: 'Saved',
+          tabBarIcon: ({color, size}) => (
+            <Ionicons name = "heart" color = {color} size = {size} />
+          ),
+        }}
+      />  
+    </Tabs>
+  );
 }
